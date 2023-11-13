3df5a7f6ee48b70471a0db1a6a085307b3676fb2
Anton V <gageviolo@gmail.com>
2023-11-13 14:05:08 +0530

    Generate slug and title with queries

--- src/app/views/actors/actors-index/actors-index.component.ts
+++ src/app/views/actors/actors-index/actors-index.component.ts
@@ -73,26 +73,39 @@ export class ActorsIndexComponent implements OnInit {
       next: queries => {
         document.body.scrollIntoView();
 
-
         let canonical = 'actors';
+        let seoTitle = 'Все актёры дорам';
 
         let page = parseInt(queries['page']);
         this.page = !isNaN(page) ? page : 1;
+
+
+        this.imagesLoaded = [];
+        this.loading = true;
+
+        let startsWith = queries['startsWith'] ?? undefined;
+        let canonicalQueries: string[] = [];
+        if (startsWith) {
+          seoTitle += '. На букву "' + startsWith + '"';
+          canonicalQueries.push('startsWith=' + startsWith);
+        }
 
         if (this.page > 1) {
           this.crumbs = [
             {path: '/actors', name: 'Актёры дорам'},
             {name: 'Стр.' + this.page},
           ];
-          canonical += '/' + this.page;
+          canonicalQueries.push('page=' + this.page)
+          seoTitle += '. Стр. ' + this.page;
         }
 
-        this.htmlS.setCanonical(canonical);
 
-        this.imagesLoaded = [];
-        this.loading = true;
-
-        let startsWith = queries['startsWith'] ?? undefined;
+        canonical += '?' + canonicalQueries.join('&');
+        console.log(canonical)
+        setTimeout(() => {
+          this.htmlS.setCanonical(canonical);
+          this.titleS.setTitle(seoTitle);
+        }, 0)
 
         this.service.getActors((this.page - 1) * this.limit, this.limit, startsWith)
           .subscribe({
