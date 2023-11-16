e656ca4d7745679084383f55ab8960988237485e
Anton V <gageviolo@gmail.com>
2023-11-16 17:57:03 +0530

    Add routes Enum

---
+++ src/app/enums/RoutesNames.ts
@@ -0,0 +1,32 @@
+export enum RNames {
+  // common
+  slug = 'slug',
+  slugParam = ':' + RNames.slug,
+
+
+  // account
+  account = 'account',
+
+  // admin
+  admin = 'admin',
+  dash = 'dashboard',
+  details = 'details',
+  edit = 'edit',
+  delete = 'delete',
+
+  // movies
+  movies = 'movies',
+  genres = 'genres',
+  collections = 'collections',
+  actors = 'actors',
+
+  // auth
+  login = 'login',
+  register = 'register',
+  forgotPassword = 'forgot-password',
+
+  // errors
+  error404 = '404',
+  videos = 'videos'
+}
+
